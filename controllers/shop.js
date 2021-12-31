472f6896e97f8f2f1f3d532d0fc26bbc73151e8b
AbdulMoeez060 <76743473+AbdulMoeez060@users.noreply.github.com>
2021-12-31 21:11:12 +0500

    added more and edited views and added constrollers

---
+++ controllers/shop.js
@@ -0,0 +1,39 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((product) => {
+    res.render("shop/product-list", {
+      prods: product,
+      pageTitle: "All Products",
+      path: "/products",
+      hasProds: product.length > 0,
+    }); //for rendering pug
+  });
+  //console.log("Another Middleware");
+  //console.log("shop js",product);//we dont do this as this will be shared in all user in the server
+  // res.sendFile(path.join(rootDir,'views','shop.html'));//for making out paths as path system is different on windows and linux
+};
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll((product) => {
+        res.render("shop/index", {
+          prods: product,
+          pageTitle: "Shop",
+          path: "/",
+          hasProds: product.length > 0,
+        }); //for rendering pug
+      });
+}
+
+exports.getCart = (req,res,next)=>{
+    res.render('shop/cart',{
+        path:'/cart',
+        pageTitle: 'Your Cart'
+    });
+}
+
+exports.getCheckout = (req,res,next)=>{
+    res.render('shop/checkout',{
+        path:'/checkout',
+        pageTitle: 'Checkout'
+    });
+}
--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,16 @@ const express= require('express');
 const path= require('path');//For joining paths
 const rootDir= require('../utils/paths')
 
-const productsController= require("../controllers/products");
+const shopsController= require("../controllers/shop");
 const router = express.Router();
 
 
-router.get('/',productsController.getProducts);
+router.get('/',shopsController.getIndex);
+
+router.get('/products',shopsController.getProducts);
+router.get('/cart',shopsController.getCart);
+router.get('/checkout',shopsController.getCheckout);
+
+
 
 module.exports = router;
