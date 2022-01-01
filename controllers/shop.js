ce501a3f2a86c16f9afd3c06d97c9f5a3fa11a40
AbdulMoeez060 <76743473+AbdulMoeez060@users.noreply.github.com>
2022-01-01 23:58:42 +0500

    added add product in cart model which adds the product into the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
+
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((product) => {
@@ -43,7 +45,10 @@ exports.getCart = (req,res,next)=>{
 
 exports.postCart = (req,res,next)=>{
   const prodId=req.body.productId;
-  console.log(prodId);
+  Product.findbyid(prodId,product=>{
+    Cart.addProduct(prodId,product.price);
+  });
+  res.redirect('/');
 }
 
 exports.getOrders = (req,res,next)=>{
--- models/cart.js
+++ models/cart.js
@@ -21,23 +21,30 @@ module.exports= class Cart{
             if (!err) {
                 cart=JSON.parse(filecontent);
             }
+            //analyze the cart to find existing products 
+            const existingProductIndex = cart.products.findIndex(prod => prod.id===id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+            //add new items / increase the quantity of existing
+
+            if (existingProduct) {
+                updatedProduct = {...existingProduct};
+                updatedProduct.qty=updatedProduct.qty+1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex]=updatedProduct;
+            }
+            else{
+                updatedProduct = {id: id,qty:1};
+                cart.products = [...cart.products,updatedProduct];
+
+            }
+            cart.totalPrice = cart.totalPrice+ +productPrice;
+            fs.writeFile(p,JSON.stringify(cart),(err)=>{
+                console.log(err);
+            })
             
         });
-        //analyze the cart to find existing products 
-        const existingProduct = cart.product.find(prod => prod.id===id);
-        let updatedProduct;
-        //add new items / increase the quantity of existing
-
-        if (existingProduct) {
-            updatedProduct = {...existingProduct};
-            updatedProduct.qty=updatedProduct.qty+1;
-        }
-        else{
-            updatedProduct = {id: id,qty:1};
-
-        }
-        cart.totalPrice = cart.totalPrice+productPrice;
-
+        
 
     }
     
