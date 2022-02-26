3bb76a2c05e270ac5e424474fef95ccdddb7e63a
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-02-26 14:16:09 +0500

    added mysql to project

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const express = require("express");
-const bodyParser= require('body-parser');
+const bodyParser = require("body-parser");
 
-const adminRoutes = require('./routes/admin');
-const expressHbs = require('express-handlebars');
-const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
+const adminRoutes = require("./routes/admin");
+const expressHbs = require("express-handlebars");
+const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 const path = require("path");
+const db = require("./utils/database");
 
 const app = express();
 
-app.engine('handlebars',expressHbs());//initialize the handle bars
+
+//app.engine("handlebars", expressHbs()); //initialize the handle bars
 
 //app.set('view engine','pug');//pug is built  in
 //app.set('view engine','handlebars');//pug is built  in
-app.set('view engine','ejs');//pug is built  in
+app.set("view engine", "ejs"); //pug is built  in
 
-app.set('views','views');
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname,'public')));//used to make css statically accessible
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/admin',adminRoutes);
+app.use(express.static(path.join(__dirname, "public"))); //used to make css statically accessible
+
+app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
