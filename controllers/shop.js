a1886e64e8ca2df3a96ec75ad1af3231f9949f16
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-02-27 23:20:06 +0500

    added the order vieew and route also completey converted the project to sql

--- controllers/shop.js
+++ controllers/shop.js
@@ -125,9 +125,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -143,14 +145,26 @@ exports.postOrder = (req, res, next) => {
         .catch((err) => console.log(err));
     })
     .then(result=>{
+      return fetchedCart.setProducts(null);
+    })
+    .then((result)=>{
       res.redirect('/orders')
+
     })
     .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+
+  req.user.getOrders({include: ['products']})
+  .then(orders=>{
+    res.render("shop/orders", {
+      path: "/orders",
+      pageTitle: "Your Orders",
+      orders:orders
+    });
+
+  })
+  .catch(err=>console.log(err));
+
 };
