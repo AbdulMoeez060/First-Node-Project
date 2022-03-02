699794bfbdf6320d57d6a89b8a051b117ef2c8d9
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-03-02 22:15:48 +0500

    fixed add to cart code

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public"))); //used to make css statically accessible
 
 app.use((req,res,next)=>{
-  User.findById("621f50de7a1a9c9545aeb98d").then(user=>{
+  User.findById("621fa3c014cda1788ca09113").then(user=>{
     req.user= new User(user.name,user.email,user.cart,user._id);
     next();
   }).catch(err=>console.log(err));
