1d99f6eca5bfc85a8ea132107839de9718c18a9b
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-04-03 14:32:15 +0500

    added a isloggedin cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get('Cookie').split(';')[4].trim().split("=")[1];
+  const isLoggedIn = req.get('Cookie').split(';')[4].trim().split("=")[1] === 'true';
     res.render("auth/login", {
       path: "/login",
       pageTitle: "Login",
